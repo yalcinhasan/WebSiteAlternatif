ad13d36f999b19a538b4c3722a170c6c276d29f1
yalci <yalci@DESKTOP-MEGPRM9>
2023-06-09 05:20:21 +0300

    Add project files.

---
+++ WebSiteAlternatif/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteAlternatif.Models.Entity;
+
+namespace WebSiteAlternatif.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        WebSiteDBEntities db = new WebSiteDBEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLHAKKIMDA.ToList();
+            return View(degerler);
+        }
+
+        public ActionResult AddMessage() { return View(); }
+
+        [HttpPost]
+        public ActionResult AddMessage(TBLILETISIM tBLILETISIM)
+        {
+            db.TBLILETISIM.Add(tBLILETISIM);
+            db.SaveChanges();//burası olmadan veri tabanına kaydetmez
+            return RedirectToAction("AddMessage");
+        }
+    }
+}
---
+++ WebSiteAlternatif/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebSiteAlternatif.MvcApplication" Language="C#" %>
